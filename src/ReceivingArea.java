7c70315bbe83c2ef85b281500b63e35c604d21e2
Ping-Lin <billy3962@hotmail.com>
2014-02-06 23:56:10 +0800

    initial

---
+++ src/ReceivingArea.java
@@ -0,0 +1,7 @@
+
+public class ReceivingArea {
+	public ReceivingArea(){
+		
+	}
+
+}
---
+++ src/ReceivingTrack.java
@@ -0,0 +1,11 @@
+/**
+ * 此Class為ReceivingArea的軌道
+ * 
+ * @author Ping
+ */
+public class ReceivingTrack {
+	
+	public ReceivingTrack(){
+		
+	}
+}
