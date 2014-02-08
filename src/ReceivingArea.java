2448da238f0681a8d8e669879ad0ad7c52f9f634
Ping-Lin <billy3962@hotmail.com>
2014-02-08 19:26:31 +0800

    create coming train function

--- src/ReceivingArea.java
+++ src/ReceivingArea.java
@@ -1,7 +1,71 @@
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
+/**
+ * 此Class為ReceivingArea，
+ * 存放軌道以及其一些設限
+ * 
+ * @author Ping
+ */
 public class ReceivingArea {
-	public ReceivingArea(){
-		
+	Track[] receivingTrack;
+	ArrayList<ArrayList<Block>> blockList;
+	private Timer timer;
+	
+	public ReceivingArea(ArrayList<ArrayList<Block>> BL){
+		receivingTrack = new Track[Constants.RECEIVING_TRACKS_NUMBER];
+		for(int i=0 ; i<receivingTrack.length ; i++)   //new 空間
+			receivingTrack[i] = new Track();
+		blockList = BL;
+		timer = new Timer();
+		timer.scheduleAtFixedRate(new ReceivingTask(), 0, 10);
 	}
-
+	/*
+	 * 假想有一個隱形的引擎把火車拉向receiving area
+	 */
+	public void moveInTrain(){
+		int j=0;
+		for(Track rt : receivingTrack){
+			for(int i=0 ; i<blockList.size() ; i++){
+				if(rt.ifEmpty==true && blockList.get(i).get(0).timeAtReceivingArea <= Test1.time){
+					rt.train = blockList.get(i);
+					for(Block b : rt.train){   //設定每一個car的receiving track為當前的track
+						b.receivingTrackNo = j;
+					}
+					rt.ifEmpty = false;
+					blockList.remove(i);
+					break;
+				}
+			}
+			j++;
+		}
+		System.out.println(findLongestTrain());
+	}
+	
+	/*
+	 * 找出最長的火車編號並回傳(int)
+	 */
+	private int findLongestTrain(){
+		int max = Integer.MIN_VALUE;
+		int maxId = -1;   //最長火車的id
+		for(Track rt : receivingTrack){
+			if(rt.ifEmpty == false){
+				if(rt.train.size() > max){
+					max = rt.train.size();
+					maxId = rt.train.get(0).trainId;
+				}
+							
+			}
+		}
+		return maxId;
+	}
+	/*
+	 * 持續做接收車子的動作
+	 */
+	class ReceivingTask extends TimerTask{
+		public void run(){
+			moveInTrain();
+		}
+	}
 }
