8a5e0c5de7cb1d4ab948f39b8b0b061d26d85c11
Ping-Lin <billy3962@hotmail.com>
2014-02-18 16:46:29 +0800

    for output result

---
+++ src/BlockToTrack.java
@@ -0,0 +1,18 @@
+/*
+ * 輸出結果用類別
+ */
+public class BlockToTrack {
+	int trackNo;
+	String blockName;
+	int dayNo;
+	double startingTime;
+	double endingTime;
+	
+	public BlockToTrack(){
+		trackNo = -1;
+		dayNo = 0;
+		startingTime = 0.0;
+		endingTime =0.0;
+	}
+	
+}
